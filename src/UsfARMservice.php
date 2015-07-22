840d99aefc2abbecf45e4efac935a4bf2a267459
James Jones <jamjon3@gmail.com>
2015-07-22 14:00:30 -0400

    Moving ARM service calls into the API module

---
+++ src/UsfARMservice.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace USF\IdM;
+
+/**
+ * Description of UsfARMservice
+ *
+ * @author james
+ */
+class UsfARMservice extends \USF\IdM\UsfARMapi {
+    /**
+     * Retrieves an array of accounts for a specified identity 
+     * 
+     * @param object $identity
+     * @return array of accounts
+     */
+    public function getAccountsForIdentityARM($identity) {
+        // return $this->getAccountsForIdentity($identity);
+        // $usfARMapi = new UsfARMapi();
+        return new JSendResponse('success', [
+            'test' => $identity,
+            // 'isset' => isset($usfARMapi),
+            'exists' => method_exists($this,"getAccountsForIdentity"),
+            // 'exists1' => method_exists($usfARMapi,"getAccountsForIdentity")
+            'data' => $this->getAccountsForIdentity($identity)
+        ]);
+    }
+}
