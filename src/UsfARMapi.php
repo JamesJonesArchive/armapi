f531dd29e858392bd3ecf839cfa0c0d5441fe113
James Jones <jamjon3@gmail.com>
2015-08-27 12:21:56 -0400

    Debugging by section

--- src/UsfARMapi.php
+++ src/UsfARMapi.php
@@ -679,42 +679,42 @@ class UsfARMapi extends UsfAbstractMongoConnection {
                 $account['state'] = [];
             }
             // Find the state indicated by the manager
-//            if(empty(\array_filter($account['state'], function($s) use($managerattributes) {
-//                return ($s['state'] == $managerattributes['state']);
-//            }))) { 
-//                $updatedattributes['state'] = (isset($account['state']))?$account['state']:[];
-//                $updatedattributes['state'][] = \array_merge($managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
-//            } else {
-//                $updatedattributes['state'] = \array_map(function($s) use($managerattributes) {
-//                    if($s['usfid'] == $managerattributes['usfid']) {
-//                        return \array_merge($s,$managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
-//                    } else {
-//                        return $s;
-//                    }
-//                },((isset($account['state']))?$account['state']:[]));
-//            }
-//            if(!isset($account['roles'])) {
-//                $account['roles'] = [];
-//            }
+            if(empty(\array_filter($account['state'], function($s) use($managerattributes) {
+                return ($s['state'] == $managerattributes['state']);
+            }))) { 
+                $updatedattributes['state'] = (isset($account['state']))?$account['state']:[];
+                $updatedattributes['state'][] = \array_merge($managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
+            } else {
+                $updatedattributes['state'] = \array_map(function($s) use($managerattributes) {
+                    if($s['usfid'] == $managerattributes['usfid']) {
+                        return \array_merge($s,$managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
+                    } else {
+                        return $s;
+                    }
+                },((isset($account['state']))?$account['state']:[]));
+            }
+            if(!isset($account['roles'])) {
+                $account['roles'] = [];
+            }
             // Iterate the role states and set those states empty as well 
             $updatedattributes['roles'] = \array_map(function($r) use($managerattributes) {
                 if(!isset($r['state'])) {
                     $r['state'] = [];
                 }
                 // Find the state indicated by the manager
-                if(empty(\array_filter($r['state'], function($s) use($managerattributes) {
-                    return ($s['state'] == $managerattributes['state']);
-                }))) { 
-                    $r['state'][] = \array_merge($managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
-                } else {
-                    $r['state'] = \array_map(function($s) use($managerattributes) {
-                        if($s['usfid'] == $managerattributes['usfid']) {
-                            return \array_merge($s,$managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
-                        } else {
-                            return $s;
-                        }
-                    },((isset($r['state']))?$r['state']:[]));
-                }
+//                if(empty(\array_filter($r['state'], function($s) use($managerattributes) {
+//                    return ($s['state'] == $managerattributes['state']);
+//                }))) { 
+//                    $r['state'][] = \array_merge($managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
+//                } else {
+//                    $r['state'] = \array_map(function($s) use($managerattributes) {
+//                        if($s['usfid'] == $managerattributes['usfid']) {
+//                            return \array_merge($s,$managerattributes,[ 'state' => '', 'timestamp' => new \MongoDate() ]);
+//                        } else {
+//                            return $s;
+//                        }
+//                    },((isset($r['state']))?$r['state']:[]));
+//                }
                 return $r;
             },((isset($account['roles']))?$account['roles']:[]));
             // Update the account
